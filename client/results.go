95c9cf3b1c5eb13a863bd419d8ba11bb301433a6
Deepti <deeptir@mit.edu>
2017-07-20 15:06:50 -0400

    Added new struct for uploading to the DB

--- client/results.go
+++ client/results.go
@@ -13,6 +13,39 @@ type CCResults struct {
 	FlowTimes  map[string][]map[string]float64 // list of times when the flows "on" started
 }
 
+/* This struct is different because it contains the results for an individual algorithm*/
+/* Used on upload to database*/
+type DBResult struct {
+	Alg        string
+	Throughput []map[float64]float64
+	Delay      map[float64]float64
+	FlowTimes  map[string][]map[string]float64
+}
+
+/*Encodes cc result struct*/
+func (res *DBResult) encode() []byte {
+	w := new(bytes.Buffer)
+	e := gob.NewEncoder(w)
+	e.Encode(res.Throughput)
+	e.Encode(res.Delay)
+	e.Encode(res.FlowTimes)
+	return w.Bytes()
+}
+
+func decodeResult(data []byte) DBResult {
+	results := DBResult{}
+	r := bytes.NewBuffer(data)
+	if data == nil || len(data) < 1 {
+		log.Error("error decoding into Result struct")
+	}
+	d := gob.NewDecoder(r)
+	d.Decode(&results.Throughput)
+	d.Decode(&results.Delay)
+	d.Decode(&results.FlowTimes)
+	return results
+
+}
+
 /*Encodes the inner CC results struct*/
 func (cc *CCResults) encode() []byte {
 	w := new(bytes.Buffer)
