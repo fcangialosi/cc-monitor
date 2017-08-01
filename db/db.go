6f690e7b6b04f693d29bec5202b6ef2d83c6a972
Deepti <deeptir@mit.edu>
2017-08-01 18:10:50 -0400

    Another small fix to parse IP file for locations correctly

--- db/db.go
+++ db/db.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"net"
 	"os"
-  "time"
-  "strings"
-  "fmt"
-  "strconv"
+	"strconv"
+	"strings"
+	"time"
 
 	"../config"
 	"../results"
@@ -16,45 +16,45 @@ import (
 )
 
 func getIPList(ip_file string) (results.IPList, int) {
-  ip_list := make(map[string](map[string][]string))
-  var num_cycles int
+	ip_list := make(map[string](map[string][]string))
+	var num_cycles int
 	file, err := os.Open(ip_file)
 	defer file.Close()
 	checkError(err)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-    ip_line := scanner.Text() // line: IP UDP alg1 alg2 TCP alg1 alg2 ....
-    ip_line_split := strings.Split(ip_line, " ")
-    log.Info(ip_line_split)
-    if len(ip_line_split) == 1 {
-      num_cycles, err = strconv.Atoi(ip_line_split[0])
-      if err != nil {
-        log.WithFields(log.Fields{"err":err}).Error("error reading num_cycles")
-      }
-      continue
-    }
-    ip := ip_line_split[0]
-    udp_algorithms := make([]string, 0)
-    tcp_algorithms := make([]string, 0)
-    udp := true
-    for _, val := range ip_line_split[3:] { // first val is IP, 2nd val is location, 3rd val is "UDP"
-      if val == "TCP" {
-        udp = false
-        continue
-      }
-      if udp {
-        udp_algorithms = append(udp_algorithms, val)
-      } else {
-        tcp_algorithms = append(tcp_algorithms, val)
-      }
-    }
+		ip_line := scanner.Text() // line: IP UDP alg1 alg2 TCP alg1 alg2 ....
+		ip_line_split := strings.Split(ip_line, " ")
+		log.Info(ip_line_split)
+		if len(ip_line_split) == 1 {
+			num_cycles, err = strconv.Atoi(ip_line_split[0])
+			if err != nil {
+				log.WithFields(log.Fields{"err": err}).Error("error reading num_cycles")
+			}
+			continue
+		}
+		ip := ip_line_split[0]
+		udp_algorithms := make([]string, 0)
+		tcp_algorithms := make([]string, 0)
+		udp := true
+		for _, val := range ip_line_split[3:] { // first val is IP, 2nd val is location, 3rd val is "UDP"
+			if val == "TCP" {
+				udp = false
+				continue
+			}
+			if udp {
+				udp_algorithms = append(udp_algorithms, val)
+			} else {
+				tcp_algorithms = append(tcp_algorithms, val)
+			}
+		}
 
-    alg_map := make(map[string][]string)
-    alg_map["UDP"] = udp_algorithms
-    alg_map["TCP"] = tcp_algorithms
+		alg_map := make(map[string][]string)
+		alg_map["UDP"] = udp_algorithms
+		alg_map["TCP"] = tcp_algorithms
 
-    ip_list[ip] = alg_map
-    log.WithFields(log.Fields{"IP": ip, "alg map": alg_map}).Info("IP LIST")
+		ip_list[ip] = alg_map
+		log.WithFields(log.Fields{"IP": ip, "alg map": alg_map}).Info("IP LIST")
 	}
 	checkError(scanner.Err())
 	return ip_list, num_cycles
@@ -65,14 +65,18 @@ func getIPLocation(ip_file string, input_ip string) string {
 	defer file.Close()
 	checkError(err)
 	scanner := bufio.NewScanner(file)
+	line_num := 0
 	for scanner.Scan() {
-    ip_line := scanner.Text() // line: IP UDP alg1 alg2 TCP alg1 alg2 ....
-    ip_line_split := strings.Split(ip_line, " ")
-    ip := ip_line_split[0]
-    location := ip_line_split[1]
-    if ip == input_ip {
-      return location
-    }
+		if line_num != 0 {
+			ip_line := scanner.Text() // line: IP UDP alg1 alg2 TCP alg1 alg2 ....
+			ip_line_split := strings.Split(ip_line, " ")
+			ip := ip_line_split[0]
+			location := ip_line_split[1]
+			if ip == input_ip {
+				return location
+			}
+		}
+		line_num++
 	}
 	checkError(scanner.Err())
 	return "NOT_FOUND"
@@ -117,7 +121,7 @@ func dbServer(ch chan results.CCResults) {
 
 	for {
 		conn, err := server.AcceptTCP()
-    log.Info("Got a report to read from the client")
+		log.Info("Got a report to read from the client")
 		if err != nil {
 			log.Warning(err)
 		}
@@ -126,16 +130,16 @@ func dbServer(ch chan results.CCResults) {
 			p := make([]byte, 15000) // large buf size
 			report_bytes := make([]byte, 0)
 			// read until client sends EOF
-      for {
-			  n, err := conn.Read(p)
-        if err == io.EOF {
-          log.Warn("client left")
-          break
-        }
-			  report_bytes = append(report_bytes, p[:n]...)
+			for {
+				n, err := conn.Read(p)
+				if err == io.EOF {
+					log.Warn("client left")
+					break
+				}
+				report_bytes = append(report_bytes, p[:n]...)
 				//log.WithFields(log.Fields{"n": n}).Info("bytes received")
-      }
-      log.Info("Finished reading the report")
+			}
+			log.Info("Finished reading the report")
 			report := results.DecodeCCResults(report_bytes)
 			report.ClientIP = conn.RemoteAddr().String() // client didn't put in right IP for some reason
 			ch <- report
@@ -146,55 +150,55 @@ func dbServer(ch chan results.CCResults) {
 
 func dbWorker(ch chan results.CCResults, ip_file string) {
 	// TODO reads jobs from channel, writes them to db
-  // db in this case is just writing a file with the results
+	// db in this case is just writing a file with the results
 
 	// read jobs from results channel forever
 	for {
 		report := <-ch
-    // spawn go routine to deal with this report
-    go func(results.CCResults) {
-      log.Info("got decoded report from channel")
-      server_ip := report.ServerIP
-      client_ip := strings.Split(report.ClientIP, ":")[0]
-      current_date := currentDate()
-      current_time := currentTime()
+		// spawn go routine to deal with this report
+		go func(results.CCResults) {
+			log.Info("got decoded report from channel")
+			server_ip := report.ServerIP
+			client_ip := strings.Split(report.ClientIP, ":")[0]
+			current_date := currentDate()
+			current_time := currentTime()
 
-      // check if everything exists
-      server_file := fmt.Sprintf("%s_logs", server_ip)
-      location := getIPLocation(ip_file, server_ip)
-      if ( location != "NOT_FOUND" ) {
-        server_file = fmt.Sprintf("%s_logs", location)
-      }
-      filename := fmt.Sprintf("%s_%s.log", client_ip, current_time)
-      path := fmt.Sprintf("exp_results/%s/%s", server_file, current_date)
-      err := os.MkdirAll(path, 0777)
-      if err != nil {
-        log.WithFields(log.Fields{"err": err, "path": path}).Panic("Creating path to store results")
-      }
-      full_path := path + "/" + filename
-      f, err := os.Create(full_path)
-      checkErrMsg(err, "creating file for path " + full_path)
-      log.WithFields(log.Fields{"path": full_path}).Info("Writing decoded results")
-      defer f.Close()
+			// check if everything exists
+			server_file := fmt.Sprintf("%s_logs", server_ip)
+			location := getIPLocation(ip_file, server_ip)
+			if location != "NOT_FOUND" {
+				server_file = fmt.Sprintf("%s_logs", location)
+			}
+			filename := fmt.Sprintf("%s_%s.log", client_ip, current_time)
+			path := fmt.Sprintf("exp_results/%s/%s", server_file, current_date)
+			err := os.MkdirAll(path, 0777)
+			if err != nil {
+				log.WithFields(log.Fields{"err": err, "path": path}).Panic("Creating path to store results")
+			}
+			full_path := path + "/" + filename
+			f, err := os.Create(full_path)
+			checkErrMsg(err, "creating file for path "+full_path)
+			log.WithFields(log.Fields{"path": full_path}).Info("Writing decoded results")
+			defer f.Close()
 
-     // marshall the struct
-     b := results.EncodeCCResults(&report)
-    checkErrMsg(err, "marshalling report into bytes")
-    _, err = f.Write(b)
-    checkErrMsg(err, "writing bytes to file")
-    }(report)
+			// marshall the struct
+			b := results.EncodeCCResults(&report)
+			checkErrMsg(err, "marshalling report into bytes")
+			_, err = f.Write(b)
+			checkErrMsg(err, "writing bytes to file")
+		}(report)
 
-  }
+	}
 }
 
 func currentDate() string {
-  _, month, day := time.Now().Date()
-  return fmt.Sprintf("%s-%d", month.String(), day)
+	_, month, day := time.Now().Date()
+	return fmt.Sprintf("%s-%d", month.String(), day)
 }
 
 func currentTime() string {
-  hour, min, sec := time.Now().Clock()
-  return fmt.Sprintf("%d.%d.%d", hour, min, sec)
+	hour, min, sec := time.Now().Clock()
+	return fmt.Sprintf("%d.%d.%d", hour, min, sec)
 }
 
 func checkError(err error) {
@@ -204,24 +208,24 @@ func checkError(err error) {
 }
 
 func checkErrMsg(err error, msg string) {
-  if err != nil {
-    log.WithFields(log.Fields{"msg": msg}).Panic(err)
-  }
+	if err != nil {
+		log.WithFields(log.Fields{"msg": msg}).Panic(err)
+	}
 }
 
 /*This file is for solely handling the database*/
 func main() {
-  log.Info(currentDate())
-  log.Info(currentTime())
-  start := time.Now()
+	log.Info(currentDate())
+	log.Info(currentTime())
+	start := time.Now()
 
-  time.Sleep(time.Second*2)
-  elapsed := time.Since(start)
-  elapsed_ms := elapsed.Seconds()* float64(time.Second/time.Millisecond)
-  log.WithFields(log.Fields{"elapsed": elapsed_ms}).Info("elapsed time")
-  // get the ip list for tests
-  ip_list, num_cycles := getIPList(config.IP_LIST_LOCATION)
-  log.WithFields(log.Fields{"num_cycles":num_cycles}).Info(ip_list)
+	time.Sleep(time.Second * 2)
+	elapsed := time.Since(start)
+	elapsed_ms := elapsed.Seconds() * float64(time.Second/time.Millisecond)
+	log.WithFields(log.Fields{"elapsed": elapsed_ms}).Info("elapsed time")
+	// get the ip list for tests
+	ip_list, num_cycles := getIPList(config.IP_LIST_LOCATION)
+	log.WithFields(log.Fields{"num_cycles": num_cycles}).Info(ip_list)
 
 	quit := make(chan struct{})
 	go introServer(config.IP_LIST_LOCATION)
