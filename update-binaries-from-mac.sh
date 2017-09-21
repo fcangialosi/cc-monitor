b79d5bd62d438e1b3b024b1f8ae202eb158067cd
Frank Cangialosi <frankc@csail.mit.edu>
2017-09-21 13:35:06 -0400

    typo in update binaries script

--- update-binaries-from-mac.sh
+++ update-binaries-from-mac.sh
@@ -4,6 +4,6 @@ SITE_SERVER=frank@nimbus2000.csail.mit.edu
 
 git pull
 scp -P 222 ./cc-client $SITE_SERVER:~/dist-site/res/cc-client-darwin
-NEW_VERSION=`grep "CLIENT_VERSION :=" client/main.go | tr '"' ' ' | awk '{print $3}'`
+NEW_VERSION=`grep "CLIENT_VERSION =" client/main.go | tr '"' ' ' | awk '{print $3 "."}'`
 echo $NEW_VERSION
-ssh -p 222 $SITE_SERVER "./update-site.sh $NEW_VERSION."
+ssh -p 222 $SITE_SERVER "./update-site.sh $NEW_VERSION"
